7e67a9c8b8813789cb26bbda840e27bcc1b7d70e
Saturn-VI <maxwang420@gmail.com>
2023-05-27 18:05:39 -0500

    added praw.Reddit()

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 #cats
-import praw as reddit
+import praw
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 client_id = 'sRQE896lrpoIOE7Z4xbb6g'
 redirect_uri = 'http://localhost:8080'
+client_secret = getenv('SECRET')
+user_agent = getenv('USER-AGENT')
+password=getenv('PASSWORD')
+username=getenv('USERNAME')
+
+reddit = praw.Reddit(
+    client_id='sRQE896lrpoIOE7Z4xbb6g',
+    client_secret=getenv('SECRET'),
+    password=getenv('PASSWORD'),
+    user_agent=getenv('USER-AGENT'),
+    username=getenv('USERNAME')
+)
