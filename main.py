c6e152d5d0c0a1367f1cd08165944959532e5386
Saturn-VI <maxwang420@gmail.com>
2023-05-27 19:34:50 -0500

    it now gets posts from a subreddit (randomly chosen out of four) and the titles+image urls

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import praw
 from dotenv import load_dotenv
 from os import getenv
+from time import sleep
+from random import randint
 
 load_dotenv()
 
@@ -10,12 +12,45 @@ redirect_uri = 'http://localhost:8080'
 client_secret = getenv('SECRET')
 user_agent = getenv('USER-AGENT')
 password=getenv('PASSWORD')
-username=getenv('USERNAME')
+username=getenv('REDDIT-USERNAME')
 
 reddit = praw.Reddit(
     client_id='sRQE896lrpoIOE7Z4xbb6g',
     client_secret=getenv('SECRET'),
     password=getenv('PASSWORD'),
     user_agent=getenv('USER-AGENT'),
-    username=getenv('USERNAME')
+    username=getenv('REDDIT-USERNAME')
 )
+
+subreddits = ['cats', 'OneOrangeBraincell', 'CatPics', 'CatLoaf']
+
+def get_cats():
+    subreddit_choice = subreddits[randint(0, 3)]
+    subreddit = reddit.subreddit(subreddit_choice)
+
+    submissions = []
+    submission_titles = []
+    submission_ids = []
+
+    for submission in subreddit.top(time_filter = "day"):
+        filtered_submission = reddit.submission(submission).url
+        print(subreddit)
+        print(submission)
+        print(submission.title)
+        print(filtered_submission)
+        submissions.append(filtered_submission)
+        submission_titles.append(submission.title)
+        submission_ids.append(submission)
+
+    index_length = randint(0,len(submissions)-1)
+    print(index_length)
+    print(submissions)
+    print(submission_ids)
+    print(submission_titles)
+
+
+while (1):
+    
+    get_cats()
+
+    input()
