ea168c8ce8305656b18193471635e7cd950bd37f
Max Wang <maxwang420@gmail.com>
2023-05-28 17:25:34 -0500

    might work better now, ux still sucks if going too fast

--- static/script.js
+++ static/script.js
@@ -1,9 +1,15 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+// function httpGet(theUrl)
+// {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, false );
+//     xmlHttp.send( null );
+//     return xmlHttp.responseText;
+// }
+
+async function httpGet (url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
 }
 
 function get_image() {
